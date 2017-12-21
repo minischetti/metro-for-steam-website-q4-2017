b1333d8496622b9c8e78e243245a06ada6254302
Dominic Minischetti III <domminischetti@gmail.com>
2017-12-20 16:20:08 -0800

    Remove styled-components. Convert styles to traditional stylesheet. Improve Steam window parity with concept and working build. Delete deprecated components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import JSZip from 'jszip'
 import JSZipUtils from 'jszip-utils'
 import FileSaver from 'file-saver'
@@ -61,7 +60,8 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="wrap">
+                <div onClick={this.downloadZip}>Download</div>
                 <ColorPicker red={this.state.red} green={this.state.green} blue={this.state.blue} updateRedValue={this.updateRedValue} updateGreenValue={this.updateGreenValue} updateBlueValue={this.updateBlueValue}></ColorPicker>
                 <Steam red={this.state.red} green={this.state.green} blue={this.state.blue} downloadZip={this.downloadZip}></Steam>
             </div>
@@ -76,32 +76,39 @@ class Steam extends React.Component {
         const red = this.props.red;
         const green = this.props.green;
         const blue = this.props.blue;
-        const Window = styled.div`
-            display: flex;
-            flex-direction: column;
-            margin: auto;
-            width: 900px;
-            height: 600px;
-            background: linear-gradient(rgba(${red}, ${green}, ${blue}, .15), black);
-            background-color: black;
-            box-shadow: 0 0 40px rgba(0,0,0,.25);
-        `
         return (
-            <Window>
-                <div onClick={this.props.downloadZip}>Download Zip</div>
-                <Header primary>
-                    <NavLink>Store</NavLink>
-                    <NavLink active>Library</NavLink>
-                    <NavLink>Communities</NavLink>
-                    <FrameButton close></FrameButton>
-                    <FrameButton></FrameButton>
-                    <FrameButton></FrameButton>
-                    <Button></Button>
-                    <Button></Button>
-                    <Button></Button>
-                </Header>
-                <Header></Header>
-            </Window>
+            <div className="window" style={{backgroundImage: `linear-gradient(rgba(${red}, ${green}, ${blue}, .15), black)`}}>
+                <div className="header">
+                    <div className="nav-button-container">
+                        <div className="nav-button">
+                            <img src="/assets/back.svg"/>
+                        </div>
+                        <div className="nav-button forward">
+                            <img src="/assets/back.svg"/>
+                        </div>
+                    </div>
+                    <span className="nav-link active" style={{ boxShadow: `inset 0 1px 0 rgb(${red}, ${green}, ${blue})`, color: `rgb(${red}, ${green}, ${blue})`}}>Store</span>
+                    <span className="nav-link">Library</span>
+                    <span className="nav-link">Community</span>
+                    <div className="button-container">
+                        <div className="button"></div>
+                        <div className="button"></div>
+                        <div className="button"></div>
+                    </div>
+                    <div className="frame-button-container">
+                        <div className="frame-button">
+                            <img src="/assets/min.svg"/>
+                        </div>
+                        <div className="frame-button">
+                            <img src="/assets/max.svg"/>
+                        </div>
+                        <div className="frame-button">
+                            <img src="/assets/close.svg"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="header secondary"></div>
+            </div>
         )
     }
 }
@@ -112,67 +119,11 @@ class ColorPicker extends React.Component {
         const green = this.props.green;
         const blue = this.props.blue;
         return (
-            <div className="slidecontainer">
+            <div className="color-picker-container">
                 <input type="range" min="1" max="255" defaultValue={red} className="slider" onInput={this.props.updateRedValue} id="red"/>
                 <input type="range" min="1" max="255" defaultValue={green} className="slider" onChange={this.props.updateGreenValue} id="green"/>
                 <input type="range" min="1" max="255" defaultValue={blue} className="slider" onChange={this.props.updateBlueValue} id="blue"/>
             </div>
-        )
-    }
-}
-
-class Button extends React.Component {
-    render() {
-        const Button = styled.div`
-            width: 30px;
-            height: 30px;
-            margin-right: 10px;
-            border-radius: 50%;
-            border: 1px solid rgba(255,255,255,.1);
-        `
-        return (
-            <Button></Button>
-        )
-    }
-}
-
-class NavLink extends React.Component {
-    render() {
-        const Link = styled.span`
-            color: ${props => this.props.active ? 'white' : 'rgba(255,255,255,.25)'};
-            font-weight: 300;
-            text-transform: uppercase;
-            margin-right: 10px;
-        `
-        return (
-            <Link>{this.props.children}</Link>
-        )
-    }
-}
-
-class Header extends React.Component {
-    render() {
-        const Header = styled.div`
-            width: 100%;
-            height: ${props => this.props.primary ? '60px' : '40px'};
-            border-bottom: 1px solid rgba(255,255,255,.1);
-        `
-        return (
-            <Header>{this.props.children}</Header>
-        )
-    }
-}
-
-class FrameButton extends React.Component {
-    render() {
-        const FrameButton = styled.div`
-            width: 10px;
-            height: 10px;
-            border-radius: 50%;
-            background-color: ${props => this.props.close ? 'red' : 'rgba(255,255,255,.1)'};
-        `
-        return (
-            <FrameButton></FrameButton>
         )
     }
 }
